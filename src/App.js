8079c9cc65ecff0e39a55c1947e29a5b61371935
Marcin Hagmajer <mhagmajer@gmail.com>
2021-02-21 10:42:34 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function LearnReactBanner({ topic }) {
-  return (
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn {topic} with WarsawJS
-    </a>
-  );
+    return (
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn {topic} with WarsawJS
+        </a>
+    );
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LearnReactBanner topic="iTunes browser in React" />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <LearnReactBanner topic="iTunes browser in React" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
