015496234f2583ca46f56ae55056313bd8c2ee44
Marcin Hagmajer <mhagmajer@gmail.com>
2021-02-21 10:36:21 +0100

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function LearnReactBanner({ topic }) {
+  return (
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn {topic} with WarsawJS
+    </a>
+  );
+}
 
 function App() {
   return (
@@ -9,14 +22,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LearnReactBanner topic="iTunes browser in React" />
       </header>
     </div>
   );
