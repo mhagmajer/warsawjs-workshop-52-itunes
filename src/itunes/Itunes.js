75adceec28401b8aa624fa94ceda678cb32038a0
Marcin Hagmajer <mhagmajer@gmail.com>
2021-02-21 11:58:52 +0100

    fetch with searchTerm

--- src/itunes/Itunes.js
+++ src/itunes/Itunes.js
@@ -20,47 +20,60 @@ import { useState } from 'react/cjs/react.development';
 
 export function Itunes() {
     const [results, setResults] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const searchTerm = 'abc';
-            const response = await fetch(
-                `https://itunes.apple.com/search?term=${encodeURIComponent(
-                    searchTerm
-                )}&entity=ebook`
-            );
-            const data = await response.json();
-
-            setResults(data.results);
-        })();
-    }, []);
+    const [searchTermInput, setSearchTermInput] = useState('');
 
     return (
         <Stack>
             <Stack direction="row">
-                <Input />
-                <Button colorScheme="blue">Search</Button>
+                <Input
+                    value={searchTermInput}
+                    onChange={(event) => setSearchTermInput(event.target.value)}
+                />
+                <Button
+                    colorScheme="blue"
+                    onClick={() => {
+                        (async () => {
+                            const response = await fetch(
+                                `https://itunes.apple.com/search?term=${encodeURIComponent(
+                                    searchTermInput
+                                )}&entity=ebook`
+                            );
+                            const data = await response.json();
+
+                            setResults(data.results);
+                        })();
+                    }}
+                >
+                    Search
+                </Button>
             </Stack>
-            <Table variant="simple">
-                <TableCaption>iTunes Ebooks</TableCaption>
-                <Thead>
-                    <Tr>
-                        <Th>Artwork</Th>
-                        <Th>Name</Th>
-                        <Th isNumeric>Price</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {results.map((result) => (
-                        <Tr>
-                            <Td>
-                                <Image src={result.artworkUrl60} />
-                            </Td>
-                            <Td>{result.trackName}</Td>
-                            <Td isNumeric>{result.price}</Td>
-                        </Tr>
-                    ))}
-                </Tbody>
-            </Table>
+            <ItunesTable results={results} />
         </Stack>
+    );
+}
+
+function ItunesTable({ results }) {
+    return (
+        <Table variant="simple">
+            <TableCaption>iTunes Ebooks</TableCaption>
+            <Thead>
+                <Tr>
+                    <Th>Artwork</Th>
+                    <Th>Name</Th>
+                    <Th isNumeric>Price</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {results.map((result) => (
+                    <Tr>
+                        <Td>
+                            <Image src={result.artworkUrl60} />
+                        </Td>
+                        <Td>{result.trackName}</Td>
+                        <Td isNumeric>{result.price}</Td>
+                    </Tr>
+                ))}
+            </Tbody>
+        </Table>
     );
 }
